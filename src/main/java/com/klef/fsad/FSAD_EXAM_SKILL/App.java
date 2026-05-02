ec330f1b5fa8c0e993bd3399f330d60397ec164b
bhanusaimedisetti <bhanusaimedisetti@gmail.com>
2026-05-03 00:21:18 +0530

    update

---
+++ src/main/java/com/klef/fsad/FSAD_EXAM_SKILL/App.java
@@ -0,0 +1,10 @@
+package com.klef.fsad.FSAD_EXAM_SKILL;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
